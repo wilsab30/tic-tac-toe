98dfdc9dfd6603edbcf323e073d5485680c4af07
Sabrina Wilbert <sabrinawilbert@gmail.com>
2016-12-12 15:29:12 -0800

    added files for player and board objects

---
+++ spec/board.spec.js
@@ -0,0 +1,30 @@
+import Board from 'board';
+
+
+describe('Board', function() {
+  describe('newGame', function() {
+    it('should create new empty game board', function() {
+
+    });
+  });
+  describe('checkWinDraw', function() {
+    it('should check arrays for posible win', function() {
+
+    });
+  });
+  describe('clearBoard', function() {
+    it('should remove all marks from board', function() {
+
+    });
+  });
+  describe('exitGame', function() {
+    it('should end game', function() {
+
+    });
+  });
+  describe('validMove', function() {
+    it('should check to see if spot alrady contains X or O', function() {
+
+    });
+  });
+});//end of board
---
+++ spec/player.spec.js
@@ -0,0 +1,29 @@
+import Player from 'player'
+
+describe('Player', function() {
+  describe('initialize', function() {
+    it('should create tow new palyers', function() {
+
+    });
+  });
+  describe('play', function() {
+    it('should mark spot with X or O', function() {
+
+    });
+  });
+  // describe('clearBoard', function() {
+  //   it('should remove all marks from board', function() {
+  //
+  //   });
+  // });
+  // describe('exitGame', function() {
+  //   it('should end game', function() {
+  //
+  //   });
+  // });
+  // describe('validMove', function() {
+  //   it('should check to see if spot alrady contains X or O', function() {
+  //
+  //   });
+  // });
+});//end of board
