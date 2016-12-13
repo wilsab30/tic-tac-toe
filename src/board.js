a1038892b74c895bb0e5664cba0e78363f2de1b5
Leah Bueing <lbueing@gmail.com>
2016-12-13 14:51:01 -0800

    can check if valid move through play function

--- src/board.js
+++ src/board.js
@@ -13,14 +13,18 @@ var Board = function() {
     8: ' ',
     9: ' '
   };
-  console.log('\n' + 'Player Board        Position Board \n\n' +
+  this.printBoard = '\n' + 'Player Board        Position Board \n\n' +
       ' ' + this.board[1] + ' | ' + this.board[2] + ' | ' + this.board[3] + '          ' + '  1 | 2 | 3 \n' +
       ' ---------' + '          ' + ' -----------\n' +
       ' ' + this.board[4] + ' | ' + this.board[5] + ' | ' + this.board[6] + '          ' + '  4 | 5 | 6 \n' +
       ' ---------' + '          ' + ' -----------\n' +
-      ' ' + this.board[7] + ' | ' + this.board[8] + ' | ' + this.board[9] + '          ' + '  7 | 8 | 9 \n');
+      ' ' + this.board[7] + ' | ' + this.board[8] + ' | ' + this.board[9] + '          ' + '  7 | 8 | 9 \n';
+  console.log(this.printBoard);
 };
 
+Board.prototype.print = function() {
+  console.log(this.printBoard);
+};
 // Board.prototype.newGame = function() {};
 
 Board.prototype.checkWinDraw = function() {
@@ -29,9 +33,9 @@ Board.prototype.checkWinDraw = function() {
 
 Board.prototype.exitGame = function() {};
 
-Board.prototype.validMove = function() {
-  if (this.board[playerSelection] == ' ')
-    this.board[playerSelection] = this.currentPlayer;
+Board.prototype.validMove = function(selection, symbol) {
+  if (this.board[selection] === ' ')
+    this.board[selection] = symbol;
 };
 
 Board.prototype.clearBoard = function() {
--- src/player.js
+++ src/player.js
@@ -3,29 +3,29 @@ import Board from 'board';
 
 
 var Player = function() {
-  var player1 = "X";
-  var player2 = "O";
-  this.currentPlayer = player2;
+  this.player1 = "X";
+  this.player2 = "O";
+  this.currentPlayer = this.player2;
 };
 
-Player.prototype.setCurrent = function() {
-  if (this.currentPlayer == player1) {
-    this.currentPlayer = player2;
+Player.prototype.play = function(selection, board) {
+  if (this.currentPlayer == this.player1) {
+    this.currentPlayer = this.player2;
   } else {
-    this.currentPlayer = player1;
+    this.currentPlayer = this.player1;
   }
+  board.validMove(selection, this.currentPlayer);
+
+  board.print();
+  // console.log('\n' +
+  //     ' ' + this.board[1] + ' | ' + this.board[2] + ' | ' + this.board[3] + '\n' +
+  //     ' ---------\n' +
+  //     ' ' + this.board[4] + ' | ' + this.board[5] + ' | ' + this.board[6] + '\n' +
+  //     ' ---------\n' +
+  //     ' ' + this.board[7] + ' | ' + this.board[8] + ' | ' + this.board[9] + '\n');
 };
 
-Player.prototype.play = function(player) {
-  board[spot] = player
 
-  console.log('\n' +
-      ' ' + this.board[1] + ' | ' + this.board[2] + ' | ' + this.board[3] + '\n' +
-      ' ---------\n' +
-      ' ' + this.board[4] + ' | ' + this.board[5] + ' | ' + this.board[6] + '\n' +
-      ' ---------\n' +
-      ' ' + this.board[7] + ' | ' + this.board[8] + ' | ' + this.board[9] + '\n');
-};
 
 
 
