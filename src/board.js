15a780d045a37af6c38d1d790a8c99bdb269791d
Leah Bueing <lbueing@gmail.com>
2016-12-13 13:39:38 -0800

    added valid move, position board, and setting current player

--- src/board.js
+++ src/board.js
@@ -10,21 +10,26 @@ var Board = function() {
     8: ' ',
     9: ' '
   };
-  console.log('\n' +
-      ' ' + this.board[1] + ' | ' + this.board[2] + ' | ' + this.board[3] + '\n' +
-      ' ---------\n' +
-      ' ' + this.board[4] + ' | ' + this.board[5] + ' | ' + this.board[6] + '\n' +
-      ' ---------\n' +
-      ' ' + this.board[7] + ' | ' + this.board[8] + ' | ' + this.board[9] + '\n');
+  console.log('\n' + 'Player Board        Position Board \n\n' +
+      ' ' + this.board[1] + ' | ' + this.board[2] + ' | ' + this.board[3] + '          ' + '  1 | 2 | 3 \n' +
+      ' ---------' + '          ' + ' -----------\n' +
+      ' ' + this.board[4] + ' | ' + this.board[5] + ' | ' + this.board[6] + '          ' + '  4 | 5 | 6 \n' +
+      ' ---------' + '          ' + ' -----------\n' +
+      ' ' + this.board[7] + ' | ' + this.board[8] + ' | ' + this.board[9] + '          ' + '  7 | 8 | 9 \n');
 };
 
 // Board.prototype.newGame = function() {};
 
-Board.prototype.checkWinDraw = function() {};
+Board.prototype.checkWinDraw = function() {
+
+};
 
 Board.prototype.exitGame = function() {};
 
-Board.prototype.validMove = function() {};
+Board.prototype.validMove = function() {
+  if (this.board[playerSelection] == ' ')
+    this.board[playerSelection] = this.currentPlayer;
+};
 
 Board.prototype.clearBoard = function() {
   for(var i = 1; i < 10; i++) {
@@ -33,5 +38,5 @@ Board.prototype.clearBoard = function() {
 };
 
 
-// socks = new Board();
-export default Board;
+socks = new Board();
+// export default Board;
--- src/player.js
+++ src/player.js
@@ -1,10 +1,19 @@
 var Player = function() {
   var player1 = "X";
   var player2 = "O";
+  this.currentPlayer = player2;
+};
+
+Player.prototype.setCurrent = function() {
+  if (this.currentPlayer == player1) {
+    this.currentPlayer = player2;
+  } else {
+    this.currentPlayer = player1;
+  }
 };
 
 Player.prototype.play = function() {
-  
+
 };
 
 
