dacf3ea18d23726d72836e13298f6f43dd4d1eb7
Sabrina Wilbert <sabrinawilbert@gmail.com>
2016-12-14 14:46:06 -0800

    Added to two test for player.

--- spec/player.spec.js
+++ spec/player.spec.js
@@ -3,12 +3,22 @@ import Player from 'player';
 describe('Player', function() {
   describe('initialize', function() {
     it('should create two new palyers', function() {
-
+      var players = new Player();
+      expect(players.player1).toBeDefined();
+    });
+    it('should create two new palyers', function() {
+      var players = new Player();
+      expect(players.player2).toBeDefined();
     });
   });
   describe('play', function() {
     it('should mark spot with X or O', function() {
-
+      var players = new Player();
+      players.play(5);
+      players.play(1);
+      players.play(4);
+      players.game.board[6] = 'X'
+  expect(players.game.board[6]).toEqual('X');
     });
   });
   // describe('clearBoard', function() {
